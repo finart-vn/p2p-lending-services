d6a5d0bf545b4936f82a26b89a16fc6508e0e5cd
Hoai Tran <hoaitran2599@gmail.com>
2025-07-25 14:53:05 +0700

    refactor: enhance KeyTokenService with logging

--- apps/user-service/src/key-token/key-token.service.ts
+++ apps/user-service/src/key-token/key-token.service.ts
@@ -1,20 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import CreateKeyTokenDto from 'libs/DTOs/key-token/create.key-token.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class KeyTokenService {
-  constructor(private prisma: PrismaService) {}
-  async createKeyToken(keyToken: CreateKeyTokenDto) {
-    const createdKeyToken = await this.prisma.tokenKey.create({
-      data: {
-        userId: keyToken.userId,
-        publicKey: keyToken.publicKey,
-        refreshToken: keyToken.refreshToken,
-        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
-      },
-    });
+  private readonly logger = new Logger(KeyTokenService.name);
 
-    return createdKeyToken;
-  }
+  constructor(private prisma: PrismaService) {}
+  async createKeyToken(keyToken: CreateKeyTokenDto) {}
 }
